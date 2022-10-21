442d9009ee7531f52ecd11c161f4d5e39cf2d363
Rahul Dev Garg <rdg042000@gmail.com>
2022-10-21 18:56:34 +0530

    initial commit

---
+++ src/main/java/io/swagger/api/ApiException.java
@@ -0,0 +1,11 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-10-21T12:36:17.472Z[GMT]")
+public class ApiException extends Exception {
+    private int code;
+
+    public ApiException(int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
